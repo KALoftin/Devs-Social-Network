d76e5bdd77f62574246eb5bc62d193bd9bae0ddf
Kyle Loftin <kaloftin85@gmail.com>
2018-11-12 21:09:54 -0800

    Added some login functionality

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb://kaloftin:the1kaloftin@ds111258.mlab.com:11258/kyle_devconnector"
+    "mongodb://kaloftin:the1kaloftin@ds111258.mlab.com:11258/kyle_devconnector",
+  secretOrKey: "secret"
 };
